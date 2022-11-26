7c1536b25b5966582ea84c74d265fb4551dcfb9f
Anisha Alexander <anisha.alexander@vonnue.com>
2022-11-26 10:17:00 +0530

    feat: personal portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+import data from './data.json' assert{type: "json"}
+const header =  document.getElementById('header')
+const liarr = data.header.nav
+console.log(data);
+
+header.innerHTML=`
+    <div class="container">
+        <nav>
+        ${
+            
+        }
+            <img src="images/namelogo.png">
+            <ul id="nav-cont">${renderHead()}
+            </ul>
+        </nav>
+  </div>
+     
+`
+
+
+renderHead()
